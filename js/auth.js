170a31e456693d13c1a7ddb1e0648bd0b3fe6325
Yigong Hu <huyg0180110559@outlook.com>
2018-01-28 00:38:57 +0800

    修改权限验证页面和脚本

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
+function auth(page, option) {
+    var au = getCookie('au');
+    var user = JSON.parse(au);
+    var privilege_key = (option) ? ["a", page, option].join("_") : ["a", page].join("_");
+    if (user[privilege_key] === 0) {
+        window.location = "/AuthError.html";
+    }
+}
