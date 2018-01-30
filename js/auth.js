156a36a99722bd53c1ef10245c07dc34a1bc1b2f
Yigong Hu <huyg0180110559@outlook.com>
2018-01-30 19:41:01 +0800

    添加权限验证

--- js/auth.js
+++ js/auth.js
@@ -6,10 +6,13 @@ function getCookie(name) {
         return null;
 }
 
-function auth(page, option) {
+function auth(page, option, redirect) {
     var user = JSON.parse(localStorage.getItem("au"));
     var privilege_key = (option) ? ["a", page, option].join("_") : ["a", page].join("_");
     if (user[privilege_key] === 0) {
-        window.location = "/AuthError.html";
+        window.location = "/AuthError.html?redirect=" + redirect ? redirect : window.location.pathname.substr(1);
+        return false;
+    } else {
+        return true;
     }
 }
