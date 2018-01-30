4ede5b059f3efe2015ecaed00ade2952d10e446e
Yigong Hu <huyg0180110559@outlook.com>
2018-01-30 20:16:38 +0800

    权限验证bug修复

--- js/auth.js
+++ js/auth.js
@@ -9,8 +9,8 @@ function getCookie(name) {
 function auth(page, option, redirect) {
     var user = JSON.parse(localStorage.getItem("au"));
     var privilege_key = (option) ? ["a", page, option].join("_") : ["a", page].join("_");
-    if (user[privilege_key] === 0) {
-        window.location = "/AuthError.html?redirect=" + redirect ? redirect : window.location.pathname.substr(1);
+    if (user[privilege_key] == 0) {
+        window.location = "../AuthError.html?redirect=" + (redirect ? redirect : window.location.pathname.substr(1));
         return false;
     } else {
         return true;
