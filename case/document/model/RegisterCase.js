9c10e0db17c90b7f32dfa61ad46a8aea8fe434da
Yigong Hu <huyg0180110559@outlook.com>
2018-02-02 16:48:27 +0800

    立案呈批表字段

--- case/document/model/RegisterCase.js
+++ case/document/model/RegisterCase.js
@@ -3,6 +3,29 @@
  * @param {Object} params 初始化参数
  */
 function RegisterCase(params) {
+    this.brief = null;
+    this.informantSex = null;
+    this.leader = null;
+    this.occurPlace = null;
+    this.examDate = null;
+    this.informantAge = null;
+    this.excutor1 = null;
+    this.occurTime = null;
+    this.informantPhone = null;
+    this.suggestion = null;
+    this.informantID = null;
+    this.reExamSuggestion = null;
+    this.content = null;
+    this.reponsible = null;
+    this.examSuggestion = null;
+    this.informantAdress = null;
+    this.reportDate = null;
+    this.informantName = null;
+    this.responsible = null;
+    this.reExamDate = null;
+    this.excuteDate = null;
+    this.objectName = null;
+    this.excutor2 = null;
     if (params) {
         for (var key in params) {
             if (params.hasOwnProperty(key)) {
