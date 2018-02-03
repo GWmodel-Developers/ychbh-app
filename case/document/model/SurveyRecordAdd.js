d392aa6fb68b4f94c1c1ccbe794af8329f7f1165
CS-Tao <whucstao@qq.com>
2018-02-04 00:06:25 +0800

    添加勘察记录

--- case/document/model/SurveyRecordAdd.js
+++ case/document/model/SurveyRecordAdd.js
@@ -13,3 +13,165 @@ function SurveyRecordAdd(params) {
         }
     }
 }
+
+SurveyRecordAdd.prototype.domMap = [
+    {
+        key: "brief",
+        name: "案由",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "startSurvey",
+        name: "勘验开始时间",
+        type: "date",
+        show: function (au) {
+            return true;
+        }
+    },
+    {
+        key: "endSurvey",
+        name: "勘验结束时间",
+        type: "date",
+        show: function (au) {
+            return true;
+        }
+    },
+    {
+        key: "place",
+        name: "勘验地点",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "instrument",
+        name: "勘验仪器",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "content",
+        name: "勘验内容",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsible",
+        name: "勘验负责人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsibleAge",
+        name: "年龄",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsibleSex",
+        name: "性别",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "excutor",
+        name: "勘验人员",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsiblePosition",
+        name: "职务",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "witness",
+        name: "当事人或见证人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "environment",
+        name: "现场环境",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "sceneCondition",
+        name: "现场实况",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "appendix",
+        name: "副页",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+]
--- case/document/model/SurveyRecordDetail.js
+++ case/document/model/SurveyRecordDetail.js
@@ -3,7 +3,23 @@
  * @param {Object} params 初始化参数
  */
 function SurveyRecordDetail(params) {
+    this.appendix = null;
     this.brief = null;
+    this.caseID = null;
+    this.content = null;
+    this.endSurvey = null;
+    this.environment = null;
+    this.excutor = null;
+    this.instrument= null;
+    this.noteId = null;
+    this.place = null;
+    this.responsible = null;
+    this.responsibleAge = null;
+    this.responsiblePosition = null;
+    this.responsibleSex = null;
+    this.sceneCondition = null;
+    this.startSurvey = null;
+    this.witness = null;
     if(params){
         for(var key in params){
             if(params.hasOwnProperty(key)){
@@ -13,3 +29,165 @@ function SurveyRecordDetail(params) {
         }
     }
 }
+
+SurveyRecordDetail.prototype.domMap = [
+    {
+        key: "brief",
+        name: "案由",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "startSurvey",
+        name: "勘验开始时间",
+        type: "date",
+        show: function (au) {
+            return true;
+        }
+    },
+    {
+        key: "endSurvey",
+        name: "勘验结束时间",
+        type: "date",
+        show: function (au) {
+            return true;
+        }
+    },
+    {
+        key: "place",
+        name: "勘验地点",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "instrument",
+        name: "勘验仪器",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "content",
+        name: "勘验内容",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsible",
+        name: "勘验负责人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsibleAge",
+        name: "年龄",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsibleSex",
+        name: "性别",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "excutor",
+        name: "勘验人员",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "responsiblePosition",
+        name: "职务",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "witness",
+        name: "当事人或见证人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "environment",
+        name: "现场环境",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "sceneCondition",
+        name: "现场实况",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "appendix",
+        name: "副页",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+]
