c899a6e58ba7da16463a5a1b480bd6dca16709db
CS-Tao <whucstao@qq.com>
2018-02-04 17:13:21 +0800

    添加案件集体讨论记录

---
+++ case/document/model/CaseDiscussion.js
@@ -0,0 +1,137 @@
+/**
+ * 案件集体讨论记录
+ * @param {Object} params 初始化参数
+ */
+function CaseDiscussion(params) {
+    brief = null;
+    discussDate = null;
+    place = null;
+    participants = null;
+    host = null;
+    reporter = null;
+    recorder = null;
+    event = null;
+    discussRecord = null;
+    suggestion = null;
+    if (params) {
+        for (var key in params) {
+            if (params.hasOwnProperty(key)) {
+                var element = params[key];
+                this[key] = element
+            }
+        }
+    }
+}
+
+CaseDiscussion.prototype.domMap = [
+    {
+        key: "brief",
+        name: "案由",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "discussDate",
+        name: "讨论时间",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "place",
+        name: "地点",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "participants",
+        name: "参加人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "host",
+        name: "主持人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "reporter",
+        name: "汇报人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "recorder",
+        name: "记录人",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "event",
+        name: "主要违法事实（证据、依据、办案程序及拟处罚意见）",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "discussRecord",
+        name: "讨论记录",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+    {
+        key: "suggestion",
+        name: "讨论意见",
+        type: "p",
+        show: function (au) {
+            return true;
+        },
+        get: function (params) {
+            return params[this.key];
+        }
+    },
+]
