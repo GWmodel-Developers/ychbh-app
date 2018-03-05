a4fef43a0b58cc0e29720f32a6fbfb4cfbfe693b
Yigong Hu <huyg0180110559@outlook.com>
2018-03-05 20:54:03 +0800

    移动端Bug修复

--- js/auth.js
+++ js/auth.js
@@ -6,7 +6,14 @@ function getCookie(name) {
         return null;
 }
 
-function auth(page, option, redirect) {
+/**
+ * 权限验证
+ * @param {String} page 特殊页面标记
+ * @param {String} option 可执行操作
+ * @param {String} redirect 重定向页面
+ * @param {number} subdir_level 子目录层级（确定在生成重定向链接时添加多少上级目录标记）
+ */
+function auth(page, option, redirect, subdir_level) {
     if (!localStorage.getItem("au")) {
         window.location = "../login.html";
         return false;
@@ -20,7 +27,7 @@ function auth(page, option, redirect) {
             var user = JSON.parse(localStorage.getItem("au"));
             var privilege_key = (option) ? ["a", page, option].join("_") : ["a", page].join("_");
             if (user[privilege_key] == 0) {
-                window.location = "/AuthError.html?redirect=" + (redirect ? redirect : window.location.pathname.substr(1));
+                window.location = "../".repeat(subdir_level ? subdir_level : 1) + "AuthError.html?redirect=" + (redirect ? redirect : window.location.pathname.substr(1));
                 return false;
             } else {
                 return true;
